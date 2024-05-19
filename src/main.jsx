20fac0e952d747ff757e6cb6d7e9a6eeeebbbc85
Wilsenz <164660254+Wilsenz@users.noreply.github.com>
2024-05-19 23:23:00 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Frontpage from './Page1.jsx'
-import Provincemenu from './Page2.jsx'
-import FoodMenu from './Food_menu_Aceh.jsx'
-import FoodMenu1 from './Food_menu_JawaBarat.jsx'
-import FoodMenu2 from './Food_menu_Bali.jsx'
-import FoodMenu3 from './Food_menu_SulawesiSelatan.jsx'
-import FoodMenu4 from './Food_menu_SumateraSelatan.jsx'
-import FoodMenu5 from './Food_menu_Maluku.jsx'
-import FoodMenu6 from './Food_menu_Yogyakarta.jsx'
+import React from "react";
+import ReactDOM from "react-dom";
+import Frontpage from "./Page1.jsx";
+import Provincemenu from "./Page2.jsx";
 import Quiz from "./Quiz_Makanan";
 import Aboutus from "./aboutus.jsx";
-import './App.css'
-import Header from './header.jsx'
-import Footer from './footer.jsx'
-import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import "./App.css";
+import Header from "./header.jsx";
+import Footer from "./footer.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import EmojipediaLinks from "./EmojipediaLinks"; // Import the EmojipediaLinks component
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Frontpage />} />  
-        <Route path="/Page2" element={<Provincemenu />} />  
-        <Route path="/Food_menu_Aceh" element={<FoodMenu />} />
-        <Route path="/Food_menu_JawaBarat" element={<FoodMenu1 />} />
-        <Route path="/Food_menu_KalimantanBarat" element={<FoodMenu2 />} />
-        <Route path="/Food_menu_SulawesiSelatan" element={<FoodMenu3 />} />
-        <Route path="/Food_menu_NTT" element={<FoodMenu4 />} />
-        <Route path="/Food_menu_Maluku" element={<FoodMenu5 />} />
-        <Route path="/Food_menu_PapuaBarat" element={<FoodMenu6 />} />
+        <Route path="/" element={<Frontpage />} />
+        <Route path="/Page2" element={<Provincemenu />} />
+        {/* Render the EmojipediaLinks component on the root route */}
+        <Route path="/" element={<EmojipediaLinks />} />
         <Route path="/Quiz_Makanan" element={<Quiz />} />
         <Route path="/aboutus" element={<Aboutus />} />
       </Routes>
       <Footer />
-    </Router>
-  </React.StrictMode>,
+    </BrowserRouter>
+  </React.StrictMode>
 );
