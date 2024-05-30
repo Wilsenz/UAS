78bbc6dd819dfeb5c7f0654edb65d4efd47d575e
Wilsenz <164660254+Wilsenz@users.noreply.github.com>
2024-05-30 16:03:00 +0700

    Update Food_menu_Aceh.jsx

--- src/Food_menu_Aceh.jsx
+++ src/Food_menu_Aceh.jsx
@@ -37,17 +37,18 @@ const FoodMenu = ({ setView, currentPage, setCurrentPage }) => {
   const [foodData, setFoodData] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  const fetchFoodData = async () => {
-    try {
-      setLoading(true);
-      const response = await axios.get("http://localhost:3000/api/food-data-aceh");
-      setFoodData(response.data);
-    } catch (error) {
-      console.error("Error fetching food data:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+app.get('/api/food-data-aceh', async (req, res) => {
+  try {
+    // Fetch data directly from your server URL
+    const response = await axios.get('https://5988-2001-448a-20a0-204a-c5bd-436d-5fd7-d417.ngrok-free.app/api/food-data-aceh');
+    const foodDataAceh = response.data;
+
+    res.json(foodDataAceh);
+  } catch (error) {
+    console.error('Error fetching food data:', error.message);
+    res.status(500).json({ error: 'Failed to fetch food data' });
+  }
+});
 
   useEffect(() => {
     fetchFoodData();
